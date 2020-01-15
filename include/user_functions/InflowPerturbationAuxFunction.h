3b144eecef203fdd2a25e8067d95c9b173d01080
grahamg3 <59615869+grahamg3@users.noreply.github.com>
2020-01-15 14:25:28 -0700

    Create InflowPerturbationAuxFunction.h

---
+++ include/user_functions/InflowPerturbationAuxFunction.h
@@ -0,0 +1,50 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2014 Sandia Corporation.                                    */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+
+#ifndef InflowPerturbationAuxFunction_h
+#define InflowPerturbationAuxFunction_h
+
+#include <AuxFunction.h>
+
+#include <vector>
+
+namespace sierra{
+namespace nalu{
+
+class InflowPerturbationAuxFunction : public AuxFunction
+{
+public:
+
+  InflowPerturbationAuxFunction(
+    const unsigned beginPos,
+    const unsigned endPos,
+    const std::vector<double> &params);
+
+  virtual ~InflowPerturbationAuxFunction() {}
+
+  virtual void do_evaluate(
+    const double * coords,
+    const double time,
+    const unsigned spatialDimension,
+    const unsigned numPoints,
+    double * fieldPtr,
+    const unsigned fieldSize,
+    const unsigned beginPos,
+    const unsigned endPos) const;
+
+private:
+  double ux_;
+  double uy_;
+  double uz_;
+  double ax_;
+  double ay_;
+  double az_;
+};
+
+} // namespace nalu
+} // namespace sierra
