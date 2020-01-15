2a673061e77c21a62643032d2e0b1566a87a8be8
grahamg3 <59615869+grahamg3@users.noreply.github.com>
2020-01-15 16:38:26 -0700

    Create RotatingWallAuxFunction.h

---
+++ include/user_functions/RotatingWallAuxFunction.h
@@ -0,0 +1,50 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2014 Sandia Corporation.                                    */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+
+#ifndef RotatingWallAuxFunction_h
+#define RotatingWallAuxFunction_h
+
+#include <AuxFunction.h>
+
+#include <vector>
+
+namespace sierra{
+namespace nalu{
+
+class RotatingWallAuxFunction : public AuxFunction
+{
+public:
+
+  RotatingWallAuxFunction(
+    const unsigned beginPos,
+    const unsigned endPos,
+    const std::vector<double> &params);
+
+  virtual ~RotatingWallAuxFunction() {}
+  
+  virtual void do_evaluate(
+    const double * coords,
+    const double time,
+    const unsigned spatialDimension,
+    const unsigned numPoints,
+    double * fieldPtr,
+    const unsigned fieldSize,
+    const unsigned beginPos,
+    const unsigned endPos) const;
+  
+private:
+  double xCentroid_;
+  double yCentroid_;
+  double MaxR_;
+  double MaxMagnitude_;
+};
+
+} // namespace nalu
+} // namespace Sierra
+
+#endif
