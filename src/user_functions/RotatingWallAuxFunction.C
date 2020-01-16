e2e44084cc97ab9e2afcebafb9bfe511dc254664
grahamg3 <59615869+grahamg3@users.noreply.github.com>
2020-01-16 16:25:16 -0700

    Better velocity algorithm

--- src/user_functions/RotatingWallAuxFunction.C
+++ src/user_functions/RotatingWallAuxFunction.C
@@ -24,17 +24,17 @@ RotatingWallAuxFunction::RotatingWallAuxFunction(
   AuxFunction(beginPos, endPos),
   xCentroid_(0.0),
   yCentroid_(0.0),
-  MaxR_(8.31),
-  MaxMagnitude_(98.21)
+  angularVelocity_(10.0),
+  zVelocity_(0.0)
 {
   if ( params.size() != 4 || params.empty() )
-    throw std::runtime_error("RotatingWallAuxFunction: requires xCentroid_, yCentroid_, MaxR_ and MaxMagnitude_");
+    throw std::runtime_error("RotatingWallAuxFunction: requires xCentroid_, yCentroid_, angularVelocity_, and zVelocity_.");
   
   // extract them
   xCentroid_ = params[0];
   yCentroid_ = params[1];
-  MaxR_ = params[2];
-  MaxMagnitude_ = params[3];
+  angularVelocity_ = params[2];
+  zVelocity_ = params[3];
 }
 
 void
@@ -53,11 +53,12 @@ RotatingWallAuxFunction::do_evaluate(
     const double x = coords[0] - xCentroid_;
     const double y = coords[1] - yCentroid_;
 
-    const double rnormalized = std::sqrt(x*x + y*y)/MaxR_;
+    const double radius = std::sqrt(x*x + y*y);
+    const double theta = std::atan2(y, x);
     
-    fieldPtr[0] = rnormalized_*MaxMagnitude_*std::cos(0.8496);
-    fieldPtr[1] = rnormalized_*MaxMagnitude_*std::sin(0.8496);
-    fieldPtr[2] = MaxMagnitude_*0.5274;
+    fieldPtr[0] = -angularVelocity_*radius*std::sin(theta);
+    fieldPtr[1] = angularVelocity_*radius*std::cos(theta);
+    fieldPtr[2] = zVelocity;
     
     fieldPtr += fieldSize;
     coords += spatialDimension;
