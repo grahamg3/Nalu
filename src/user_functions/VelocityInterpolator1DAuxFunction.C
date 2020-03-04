a43745a1ca09041705288801ac86bb6bdd5b76e5
grahamg3 <59615869+grahamg3@users.noreply.github.com>
2020-03-04 10:10:16 -0700

    Finished first draft algorithm, needs debugging

--- src/user_functions/VelocityInterpolator1DAuxFunction.C
+++ src/user_functions/VelocityInterpolator1DAuxFunction.C
@@ -33,7 +33,8 @@ VelocityInterpolator1DAuxFunction::VelocityInterpolator1DAuxFunction(
   maxpos_(0.0),
   pos_({0.0, 0.0, 0.0}),
   // velocities array
-  vel_({{0.0, 0.0, 0.0},{0.0, 0.0, 0.0}})
+  vel_({{0.0, 0.0, 0.0},{0.0, 0.0, 0.0}}),
+  defaultvel_({0.0,0.0,0.0})
 {
   //extract the parameters
   if ( params.size() != 6 || params.empty() )
@@ -50,8 +51,13 @@ VelocityInterpolator1DAuxFunction::VelocityInterpolator1DAuxFunction(
   r_ = (params[0] == 4);
   minpos_ = params[4];
   maxpos_ = params[params.size() - 4];
-  for(unsigned n=0; n < params.size(); ++n) {
+  for(unsigned n=4; n < params.size(); n += 4) {
+    pos_[n/4-1] = params[n];
+    vel_[n/4-1][0] = params[n+1];
+    vel_[n/4-1][1] = params[n+2];
+    vel_[n/4-1][2] = params[n+3];
   }
+  defaultvel_ = {params[1], params[2], params[3]};
 }
 
 void
@@ -66,15 +72,47 @@ VelocityInterpolator1DAuxFunction::do_evaluate(
   const unsigned /*endPos*/) const
 {
   for(unsigned p=0; p < numPoints; ++p) {
+    
+    // initialize variables at point
+    double pointvelocity_[3] = defaultvel_;
+    const double xp = coords[0];
+    const double yp = coords[1];
+    const double zp = coords[2];
+    const double radius = std::sqrt(xp*xp + yp*yp);
+    const double theta = std::atan2(yp, xp);
 
-    // seed the random functions for this node at this time
-    std::srand(time*1e8);
-    std::srand(std::rand()*(p+1)*time*1e4);
-
-    // calculate velocity components
-    fieldPtr[0] = ux_ + 2*((std::rand()-0.1)/RAND_MAX - 0.5)*ax_;
-    fieldPtr[1] = uy_ + 2*((std::rand()-0.1)/RAND_MAX - 0.5)*ay_;
-    fieldPtr[2] = uz_ + 2*((std::rand()-0.1)/RAND_MAX - 0.5)*az_;
+    // find the value of the direction used for index
+    const double index_ = x_*xp + y_*yp + z_*zp + r_*(radius);
+    
+    // interpolate velocities using index
+    if ( index_ < minpos || index > maxpos ) {
+      pointvelocity_ = defaultvel_;
+    } else {
+      bool passed_ = false;
+      for(unsigned m=0; m < pos_.size(); ++m) {
+        if(index_ == pos_[m]) {
+          pointvelocity_ = {vel_[m][0], vel_[m][1], vel_[m][2]};
+        } else if(index_ > pos_[m] && passed_ == false) {
+          passed_ = true;
+          double ratio_ = (index_ - pos_[m])/(pos_[m+1] - pos[m]);
+          double comp1_ = vel_[m][0] + ratio_*(vel_[m+1][0] - vel_[m][0]);
+          double comp2_ = vel_[m][1] + ratio_*(vel_[m+1][1] - vel_[m][1]);
+          double comp3_ = vel_[m][2] + ratio_*(vel_[m+1][2] - vel_[m][2]);
+          pointvelocity_ = {comp1_, comp2_, comp3_};
+        }
+      }
+    }
+    
+    // transform results into correct coordinates and apply to field
+    if ( r_ == 1 ) {
+      fieldPtr[0] = pointvelocity_[0]*cos(theta) - pointvelocity_[1]*radius*std::sin(theta);
+      fieldPtr[1] = pointvelocity_[0]*sin(theta) + pointvelocity_[1]*radius*std::cos(theta);
+      fieldPtr[2] = pointvelocity_[2];
+    } else {
+      fieldPtr[0] = pointvelocity_[0]
+      fieldPtr[1] = pointvelocity_[1];
+      fieldPtr[2] = pointvelocity_[2];
+    }
 
     fieldPtr += fieldSize;
     coords += spatialDimension;
