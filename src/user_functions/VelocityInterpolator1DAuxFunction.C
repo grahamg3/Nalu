f65bb53c1bbdbe985cf034a19c57a87ba7ac1f79
grahamg3 <59615869+grahamg3@users.noreply.github.com>
2020-03-09 09:07:44 -0600

    New params_ vector

--- src/user_functions/VelocityInterpolator1DAuxFunction.C
+++ src/user_functions/VelocityInterpolator1DAuxFunction.C
@@ -30,7 +30,9 @@ VelocityInterpolator1DAuxFunction::VelocityInterpolator1DAuxFunction(
   r_(0),
   // position limits
   minpos_(0.0),
-  maxpos_(0.0)
+  maxpos_(0.0),
+  // parameters copy
+  params_(params)
 
 {
   //extract the parameters
@@ -48,6 +50,7 @@ VelocityInterpolator1DAuxFunction::VelocityInterpolator1DAuxFunction(
   r_ = (params[0] == 4);
   minpos_ = params[4];
   maxpos_ = params[params.size() - 4];
+  params_ = params;
 }
 
 void
@@ -64,9 +67,9 @@ VelocityInterpolator1DAuxFunction::do_evaluate(
   for(unsigned p=0; p < numPoints; ++p) {
     
     // initialize variables at point
-    double v0 = params[1];
-    double v1 = params[2];
-    double v2 = params[3];
+    double v0 = params_[1];
+    double v1 = params_[2];
+    double v2 = params_[3];
     const double xp = coords[0];
     const double yp = coords[1];
     const double zp = coords[2];
@@ -78,22 +81,22 @@ VelocityInterpolator1DAuxFunction::do_evaluate(
     
     // interpolate velocities using index
     if ( index < minpos_ || index > maxpos_ ) {
-      v0 = params[1];
-      v1 = params[2];
-      v2 = params[3];
+      v0 = params_[1];
+      v1 = params_[2];
+      v2 = params_[3];
     } else {
       bool passed = false;
-      for(unsigned n=4; n < params.size(); n += 4) {
-        if(index == params[n]) {
-          v0 = params[n+1];
-          v1 = params[n+2];
-          v2 = params[n+3];
-        } else if(index > params[n] && passed == false) {
+      for(unsigned n=4; n < params_.size(); n += 4) {
+        if(index == params_[n]) {
+          v0 = params_[n+1];
+          v1 = params_[n+2];
+          v2 = params_[n+3];
+        } else if(index > params_[n] && passed == false) {
           passed = true;
-          double ratio = (index - params[n])/(params[n+4] - params[n]);
-          v0 = params[n+1] + ratio*(params[n+5] - params[n+1]);
-          v1 = params[n+2] + ratio*(params[n+6] - params[n+2]);
-          v2 = params[n+3] + ratio*(params[n+7] - params[n+3]);
+          double ratio = (index - params_[n])/(params_[n+4] - params_[n]);
+          v0 = params_[n+1] + ratio*(params_[n+5] - params_[n+1]);
+          v1 = params_[n+2] + ratio*(params_[n+6] - params_[n+2]);
+          v2 = params_[n+3] + ratio*(params_[n+7] - params_[n+3]);
         }
       }
     }
